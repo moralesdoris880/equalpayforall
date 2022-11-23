59c90995c03116ce8badb302c909ac3120d336a4
Doris Morales <moralesdoris880@gmail.com>
2022-11-23 14:17:02 -0500

    change date in jobinfo

--- db/migrate/20221123050707_create_jobinfos.rb
+++ db/migrate/20221123050707_create_jobinfos.rb
@@ -5,7 +5,9 @@ class CreateJobinfos < ActiveRecord::Migration[7.0]
       t.string :job_title
       t.string :company_name
       t.string :type_of_payment
-      t.int :years_of_employment
+      # t.int :years_of_employment Change to MMYYYY FORMAT
+      t.int :employment_start
+      t.int :employment_end
       t.text :job_review
       t.timestamps
     end
