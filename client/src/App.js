0a62139e3098dc127f5e95953e5ad5360797c64d
Doris Morales <moralesdoris880@gmail.com>
2022-11-24 22:46:21 -0500

    job component added with fetches

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setUser(user)
+        });
+      }
+    });
+  }, []);
+
  return(
   <BrowserRouter>
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/about" element={<About />}/>
-        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<Home user={user} />}/>
       </Routes>
     </div>
   </BrowserRouter>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,40 @@
-function Home() {
+import { useState, useEffect } from "react";
+
+function Home({user}) {
+  const[jobs,setJobs]=useState([])
+  const[userjobs,setUserjobs]=useState([])
+    
+  useEffect(() => {
+      fetch("/drinks").then((r) => {
+        if (r.ok) {
+          r.json().then((drinks) => setTopDrinks(drinks));
+        }
+      });
+    }, []);
+    
+    useEffect(() => {
+        fetch("/jobjinfos").then((r) => {
+          if (r.ok) {
+            r.json().then((jobinfos) => setJobs(jobinfos));
+          }
+        });
+      }, []);
+
+
+  if(!user)return(
+    <div>
+      
+    </div>);
+  else 
     return (
       <div>
-        <p>Welcome Back ENTER NAME</p>
+        <p>Welcome Back {user.username}</p>
         <div>
           <p>My Jobs</p>
           <button>Enter New Job</button>
-          {/* List All User Jobs */}
+          <div>
+            {userjobs.map((userjob) => <Job userjob={userjob} user={user}/>)}
+          </div>
         </div>
         <div>
           <p>See New Job Reviews</p>
